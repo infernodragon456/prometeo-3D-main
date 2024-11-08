c8220bd1f20cfc2efd1d65b361fb2c2b93bab9f5
infernodragon456 <vighumandy420@gmail.com>
2024-11-08 16:20:14 +0530

    Build 3D site

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ScrollControls } from "@react-three/drei";
+import { EffectComposer } from "@react-three/postprocessing";
+import Overlay from "./components/Overlay";
+import { usePlay } from "./components/Play";
 
 function App() {
+  const {play, end} = usePlay()
   return (
     <>
-      <Canvas camera={{
-        position: [0, 0, 5],
-        fov: 30,
-      }}>
+      <Canvas >
         <color attach="background" args={["#ececec"]} />
-        <Experience />
+        <ScrollControls pages={play && !end ? 20 : 0}
+          damping={0.5}
+          style={{
+            top: "10px",
+            left: "0px",
+            bottom: "10px",
+            right: "10px",
+            width: "auto",
+            height: "auto",
+            animation: "fadeIn 2.4s ease-in-out 1.2s forwards",
+            opacity: 0,
+          }} >
+          <Experience />
+        </ScrollControls>
+        <EffectComposer>
+          {/* <Noise opacity={0.1} /> */}
+        </EffectComposer>
       </Canvas>
+      <Overlay/>
     </>
   );
 }
